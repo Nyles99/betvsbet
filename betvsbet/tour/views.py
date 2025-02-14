22f03e1503b37066d6455e3a5037895ad68cb706
Nikita Molchanov <nikita@PARCNEW>
2025-02-14 16:43:25 +0300

    Прописал Mixin

--- betvsbet/tour/views.py
+++ betvsbet/tour/views.py
@@ -4,68 +4,24 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse, reverse_lazy
 from django.template.loader import render_to_string
 from django.views import View
-from django.views.generic import TemplateView, ListView, DetailView, FormView
+from django.views.generic import TemplateView, ListView, DetailView, FormView, CreateView, UpdateView
+
+from betvsbet.tour.utils import DataMixin
 
 from .forms import AddPostForm, UploadFileForm
 
 from .models import Category, Tour, TagPost, UploadFiles
 
-menu = [{'title': "О сайте", 'url_name': 'about'},
-        {'title': "Добавить турнир", 'url_name': 'add_page'},
-        {'title': "Обратная связь", 'url_name': 'contact'},
-        {'title': "Войти", 'url_name': 'login'},
-]
 
-
-
-
-
-"""def index(request): #HttpRequest
-    posts = Tour.published.all().select_related('cat')
-    data = {
-        'title': 'Главная страница',
-        'menu': menu,
-        'posts': posts,
-        'cat_selected': 0,
-        }
-    return render(request, 'tour/index.html', context=data)"""
-
-
-# def handle_uploaded_file(f):
-#    with open(f"uploads/{f.name}", "wb+") as destination:
-#        for chunk in f.chunks():
-#            destination.write(chunk)
-
-
-class TourHome(ListView):
+class TourHome(DataMixin, ListView):
     # model = Tour
     template_name = 'tour/index.html'
     context_object_name = 'posts'
-    extra_context = {
-        'title': 'Главная страница',
-        'menu': menu,
-        'cat_selected': 0,
-    }
-    
+    title_page = "Главная страница"
+    cat_selected = 0
+   
     def get_queryset(self):
         return Tour.published.all().select_related('cat')
-    
-    
-    """template_name = 'tour/index.html'
-    extra_context = {
-        'title': 'Главная страница',
-        'menu': menu,
-        'posts': Tour.published.all().select_related('cat'),
-        'cat_selected': 0,
-    }"""
-    
-    """def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
-        context['menu'] = menu
-        context['posts'] = Tour.published.all().select_related('cat')
-        context['cat_selected'] = int(self.request.GET.get('cat_id', 0))
-        return context"""
     
 
 def about(request):
@@ -81,95 +37,32 @@ def about(request):
                   {'title': 'О сайте', 'menu': menu, 'form': form})
 
 
-# def show_post(request, post_slug):
-#    post = get_object_or_404(Tour, slug=post_slug)
-#    
-#    data = {
-#        'title': 'Главная страница',
-#        'menu': menu,
-#        'post': post,
-#        'cat_selected': 1,
-#        }
-#    
-#    return render(request, 'tour/post.html', data)
-
-class ShowPost(DetailView):
-    # model = Tour
+class ShowPost(DataMixin, DetailView):
     template_name = 'tour/post.html'
     slug_url_kwarg = 'post_slug'
     context_object_name = 'post'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = context['post'].title
-        context['menu'] = menu
-        return context
+        return self.get_mixin_context(context, title=context['post'].title)
     
     def get_object(self, queryset=None):
         return get_object_or_404(Tour.published, slug=self.kwargs[self.slug_url_kwarg])
 
-
-#def add_page(request):
-#    if request.method == 'POST':    
-#        form = AddPostForm(request.POST, request.FILES)
-#        if form.is_valid():
-#        # print()
-#        #    try:
-#        #        Tour.objects.create(**form.cleaned_data)
-#        #        return redirect('home')
-#        #    except:
-#        #        form.add_error(None, "Ошибка создания турнира")"""
-#            form.save()
-#            return redirect('home')
-#    else:
-#        form = AddPostForm()
-
-#    data = {
-#        'menu': menu,
-#        'title': 'Добавление турнира',
-#        'form': form
         
-#    }
-#    return render(request, 'tour/addpage.html', data)
-
-
-"""class AddPage(View):
-    def get(self, request):
-        form = AddPostForm()
-        data = {
-        'menu': menu,
-        'title': 'Добавление турнира',
-        'form': form
-        
-        }
-        return render(request, 'tour/addpage.html', data)
-    
-    def post(self, request):
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-        
-        data = {
-            'menu': menu,
-            'title': 'Добавление турнира',
-            'form': form
-            
-        }
-        return render(request, 'tour/addpage.html', data)"""
-        
-class AddPage(FormView):
+class AddPage(DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'tour/addpage.html'
-    success_url = reverse_lazy('home')
-    extra_context = {
-        'menu': menu,
-        'title': 'Добавление турнира',
-    }
+    title_page = 'Добавление турнира'
     
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+    
+class UpdatePage(DataMixin, UpdateView):
+    
+    model = Tour
+    fields = ['title', 'content','photo', 'is_published', 'cat']
+    template_name = 'tour/addpage.html'
+    success_url = reverse_lazy('home')
+    title_page = 'Редактирование турнира'
     
 
 def contact(request):
@@ -191,7 +84,7 @@ def show_category(request, cat_slug):
     return render(request, 'tour/index.html', data)
 
 
-class TourCategory(ListView):
+class TourCategory(DataMixin, ListView):
     template_name = 'tour/index.html'
     context_object_name ='posts'
     allow_empty = False
@@ -213,19 +106,6 @@ class TourCategory(ListView):
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
-
-# def show_tag_postlist(request, tag_slug):
-#     tag = get_object_or_404(TagPost, slug=tag_slug)
-#     posts = tag.tags.filter(is_published=Tour.Status.PUBLISHED).select_related('cat')
-#     
-#     data = {
-#         'title': f"Тег: {tag.tag}",
-#         'menu': menu,
-#         'posts': posts,
-#         'cat_selected': None,
-#         }
-#     
-#     return render(request, 'tour/index.html', data)
 
 
 class TagPostList(ListView):
